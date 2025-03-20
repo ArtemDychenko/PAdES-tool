fe257cf5966ae1326766a2858816dfed54e4130a
Artem Dychenko <s192441@student.pg.edu.pl>
2025-03-20 13:49:58 +0100

    mod: using qfiledialog instead qtreeview

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTreeView
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTreeView, QFileDialog
 from PyQt6.QtGui import QTextCursor, QFileSystemModel
 from PyQt6.QtCore import QSortFilterProxyModel, Qt, QDir, QModelIndex
 
@@ -7,51 +7,51 @@ import os
 import sys
 
 
-class PdfFilterProxyModel(QSortFilterProxyModel):
-    def __init__(self):
-        super().__init__()
-
-    def filterAcceptsRow(self, source_row, source_parent):
-        """
-                Returns true if the item in the row indicated by the given source_row
-                and source_parent should be included in the model; otherwise returns false.
-
-                Parameters:
-                    source_row – int
-                    source_parent – PySide2.QtCore.QModelIndex
-
-                Returns:
-                    bool: True if the item should be included in the model, False otherwise.
-                """
-        model = self.sourceModel()
-        index = model.index(source_row, 0, source_parent)
-
-        if model.isDir(index):
-            directory_path = model.filePath(index)
-            return self.contains_pdf(directory_path)
-        else:
-            return model.filePath(index).lower().endswith(".pdf")
-
-    def contains_pdf(self, directory_path):
-        """
-                Returns true if the directory contains pdf file; otherwise returns false.
-
-                Parameters:
-                    directory_path -`str`
-
-                Returns:
-                    bool: True if there is pdf file, False otherwise.
-                """
-        iterator = QDir(directory_path).entryInfoList(
-            filters=QDir.Filter.Files | QDir.Filter.AllDirs | QDir.Filter.NoDotAndDotDot,
-            sort=QDir.SortFlag.Name,
-        )
-        for entity in iterator:
-            if entity.isDir():
-                if self.contains_pdf(entity.filePath()): return True
-            elif entity.suffix().lower() == "pdf":
-                return True
-        return False
+# class PdfFilterProxyModel(QSortFilterProxyModel):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def filterAcceptsRow(self, source_row, source_parent):
+#         """
+#                 Returns true if the item in the row indicated by the given source_row
+#                 and source_parent should be included in the model; otherwise returns false.
+#
+#                 Parameters:
+#                     source_row – int
+#                     source_parent – PySide2.QtCore.QModelIndex
+#
+#                 Returns:
+#                     bool: True if the item should be included in the model, False otherwise.
+#                 """
+#         model = self.sourceModel()
+#         index = model.index(source_row, 0, source_parent)
+#
+#         if model.isDir(index):
+#             directory_path = model.filePath(index)
+#             return self.contains_pdf(directory_path)
+#         else:
+#             return model.filePath(index).lower().endswith(".pdf")
+#
+#     def contains_pdf(self, directory_path):
+#         """
+#                 Returns true if the directory contains pdf file; otherwise returns false.
+#
+#                 Parameters:
+#                     directory_path -`str`
+#
+#                 Returns:
+#                     bool: True if there is pdf file, False otherwise.
+#                 """
+#         iterator = QDir(directory_path).entryInfoList(
+#             filters=QDir.Filter.Files | QDir.Filter.AllDirs | QDir.Filter.NoDotAndDotDot,
+#             sort=QDir.SortFlag.Name,
+#         )
+#         for entity in iterator:
+#             if entity.isDir():
+#                 if self.contains_pdf(entity.filePath()): return True
+#             elif entity.suffix().lower() == "pdf":
+#                 return True
+#         return False
 
 
 class MainIU(QMainWindow):
@@ -59,36 +59,41 @@ class MainIU(QMainWindow):
         super(MainIU, self).__init__()
         loadUi("design.ui", self)
 
-        ## implementation of files view
-        self.model = QFileSystemModel()
-
-        home_directory = os.path.expanduser("~")
-        self.model.setRootPath(home_directory)
-
-        self.filterProxyModel = PdfFilterProxyModel()
-        self.filterProxyModel.setSourceModel(self.model)
-
-        self.treeView.setModel(self.filterProxyModel)
-
-        self.treeView.setRootIndex(self.filterProxyModel.mapFromSource(self.model.index(home_directory)))
 
         ## implementation of buttons
         self.signButton.clicked.connect(self.sign_click_handler)
         self.verificationButton.clicked.connect(self.verify_click_handler)
 
+        ##implementation of actions in menu
+        self.actionChoose_file.triggered.connect(self.action_choose_file_handler)
+        self.actionexit.triggered.connect(self.close)
+
     def sign_click_handler(self):
         self.add_log("PDF has been signed")
+
 
     def verify_click_handler(self):
         self.add_log("PDF has been verified")
 
+
     def add_log(self, message):
         self.textBrowser.append(message)
-
         cursor = self.textBrowser.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.End)
         self.textBrowser.setTextCursor(cursor)
         self.textBrowser.ensureCursorVisible()
+
+
+    def action_choose_file_handler(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Open File")
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            self.add_log("Selected File:"+selected_files[0])
+
 
 
 if __name__ == "__main__":
