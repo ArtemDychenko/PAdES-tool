c4b910ff75f5bd81f95ca679757b6f3edf484a5b
Artem Dychenko <s192441@student.pg.edu.pl>
2025-03-21 19:37:40 +0100

    mod: review changes

--- app/main.py
+++ app/main.py
@@ -12,15 +12,15 @@ class MainIU(QMainWindow):
         loadUi("design.ui", self)
 
         # implementation of buttons
-        self.signButton.clicked.connect(self.sign_click_handler)
-        self.verificationButton.clicked.connect(self.verify_click_handler)
+        self.sign_button.clicked.connect(self.sign_click_handler)
+        self.verification_button.clicked.connect(self.verify_click_handler)
 
         # implementation of actions in menu
-        self.actionChoose_pdf_file.triggered.connect(
+        self.action_choose_pdf_file.triggered.connect(
             self.action_choose_pdf_file_handler
         )
-        self.actionexit.triggered.connect(self.close)
-        self.actionChoose_public_key.triggered.connect(
+        self.action_exit.triggered.connect(self.close)
+        self.action_choose_public_key.triggered.connect(
             self.action_choose_public_key_handler
         )
 
@@ -31,11 +31,11 @@ class MainIU(QMainWindow):
         self.add_log("PDF has been verified")
 
     def add_log(self, message: str) -> None:
-        self.textBrowser.append(message)
-        cursor = self.textBrowser.textCursor()
+        self.text_browser.append(message)
+        cursor = self.text_browser.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.End)
-        self.textBrowser.setTextCursor(cursor)
-        self.textBrowser.ensureCursorVisible()
+        self.text_browser.setTextCursor(cursor)
+        self.text_browser.ensureCursorVisible()
 
     def action_choose_pdf_file_handler(self) -> None:
         self.add_log(self.choose_file("PDF files(*.pdf)"))
@@ -45,7 +45,7 @@ class MainIU(QMainWindow):
 
     def choose_file(self, name_filter: str) -> Optional[str]:
         file_dialog = QFileDialog(self)
-        file_dialog.setWindowTitle("Open File")
+        file_dialog.setWindowTitle("Choose File")
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
         file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
         file_dialog.setNameFilter(name_filter)
