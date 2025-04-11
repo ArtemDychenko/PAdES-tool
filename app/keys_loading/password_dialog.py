fb337c1ed8d780c7585aed3adda86550ddae78f2
Artem Dychenko <s192441@student.pg.edu.pl>
2025-04-11 11:50:26 +0200

    feat: add password dialog styling

--- app/keys_loading/password_dialog.py
+++ app/keys_loading/password_dialog.py
@@ -1,14 +1,30 @@
-from PyQt6.QtWidgets import QDialog, QDialogButtonBox, QLabel, QLineEdit, QGridLayout
+import os
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QDialog,
+    QDialogButtonBox,
+    QLabel,
+    QLineEdit,
+    QGridLayout,
+    QVBoxLayout,
+    QWidget,
+    QApplication,
+)
 
 
 class PasswordDialog(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Enter Password")
-        self.setFixedSize(300, 70)
+        self.setWindowTitle(" ")
+        self.setMinimumWidth(200)
 
-        QBtn = QDialogButtonBox.StandardButton.Ok
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        stylesheet_path = os.path.join(base_dir, "styles.qss")
+
+        with open(stylesheet_path, "r") as stylesheet:
+            self.setStyleSheet(stylesheet.read())
 
         self.message = QLabel("Write private key password")
 
@@ -16,13 +32,13 @@ class PasswordDialog(QDialog):
         self.input.setClearButtonEnabled(True)
         self.input.setEchoMode(QLineEdit.EchoMode.Password)
 
-        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
         self.buttonBox.accepted.connect(self.accept)
 
-        layout = QGridLayout()
-        layout.addWidget(self.message, 0, 0, 1, 2)
-        layout.addWidget(self.input, 1, 0, 1, 1)
-        layout.addWidget(self.buttonBox, 1, 1, 1, 1)
+        layout = QVBoxLayout()
+        layout.addWidget(self.message)
+        layout.addWidget(self.input)
+        layout.addWidget(self.buttonBox, alignment=Qt.AlignmentFlag.AlignCenter)
         self.setLayout(layout)
 
     def get_password(self):
