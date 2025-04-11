c44c5c37b52fa3a280738b8b1699f6a426c47654
Artem Dychenko <s192441@student.pg.edu.pl>
2025-04-11 09:05:28 +0200

    add keys loading module and implement dialog functionality

---
+++ app/keys_loading/__init__.py
@@ -0,0 +1,4 @@
+from .private_key_loading import PrivateKey
+from .public_key_loading import PublicKey
+
+__all__ = ["PrivateKey", "PublicKey"]
--- app/main.py
+++ app/main.py
@@ -4,16 +4,56 @@ from typing import Optional
 
 from PyQt6.QtCore import QTimer
 from PyQt6.QtGui import QTextCursor
-from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QFileDialog,
+    QDialog,
+    QDialogButtonBox,
+    QLabel,
+    QLineEdit,
+    QGridLayout,
+)
 from PyQt6.uic import loadUi
 
+from app.keys_loading import PrivateKey, PublicKey
 from pendrive_detection import PenDriveFinder
+
+
+class PasswordDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Enter Password")
+        self.setFixedSize(300, 70)
+
+        QBtn = QDialogButtonBox.StandardButton.Ok
+
+        self.message = QLabel("Write private key password")
+
+        self.input = QLineEdit()
+        self.input.setClearButtonEnabled(True)
+        self.input.setEchoMode(QLineEdit.EchoMode.Password)
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+
+        layout = QGridLayout()
+        layout.addWidget(self.message, 0, 0, 1, 2)
+        layout.addWidget(self.input, 1, 0, 1, 1)
+        layout.addWidget(self.buttonBox, 1, 1, 1, 1)
+        self.setLayout(layout)
+
+    def get_password(self):
+        return self.input.text()
 
 
 class MainIU(QMainWindow):
     def __init__(self):
         super(MainIU, self).__init__()
         self.detector = PenDriveFinder()
+        self.private_key = PrivateKey()
+        self.public_key = PublicKey()
         ui_path = os.path.join(os.path.dirname(__file__), "design.ui")
         loadUi(ui_path, self)
 
@@ -32,7 +72,7 @@ class MainIU(QMainWindow):
 
         # implementation of pendrive detection
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.find_private_key_path)
+        self.timer.timeout.connect(self.check_pendrive)
         self.timer.start(3000)
 
     def sign_click_handler(self) -> None:
@@ -54,24 +94,35 @@ class MainIU(QMainWindow):
     def action_choose_public_key_handler(self) -> None:
         self.add_log(self.choose_file("Public key files(*.pem)"))
 
-    def find_private_key_path(self) -> None:
+    def check_pendrive(self) -> None:
         pen_drives = self.detector.find_all_pen_drives()
 
         if len(pen_drives) != 0:
-            self.add_log("Pendrive has been found")
-            for pen_drive in pen_drives:
-                self.add_log(pen_drive)
+            if self.private_key.get_private_key() is None:
+                self.add_log("Pendrive has been detected")
 
-            pen_drive = self.detector.find_pen_drive_with_private_key(pen_drives)
+                pen_drive = self.detector.find_pen_drive_with_private_key(pen_drives)
 
-            if pen_drive is None:
-                self.add_log("Private key has not been found")
+                if pen_drive is None:
+                    self.add_log("Private key has not been found")
+                    self.private_key.reset_private_key()
 
-            if pen_drive is not None:
-                self.add_log("Private key has been found")
-                print(self.detector.get_private_key_path(pen_drive))
+                if pen_drive is not None:
+                    if self.private_key.get_private_key() is None:
+                        self.add_log("Private key has been found")
 
+                        dlg = PasswordDialog()
+                        if dlg.exec() == QDialog.DialogCode.Accepted:
+                            password = dlg.get_password()
+                            print(password)
+                            self.private_key.load_private_key(
+                                self.detector.get_private_key_path(pen_drive), password
+                            )
 
+                        print(self.private_key.get_private_key())
+        else:
+            self.add_log("Pendrive has not been detected")
+            self.private_key.reset_private_key()
 
 
 def choose_file(self, name_filter: str) -> Optional[str]:
